f854a5ae6b3a22b0e7a394d5fd8a00ecb93244e2
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2023-11-23 13:51:40 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,26 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+
+using namespace std;
+
+int recursion1(int A, int B) {
+    if (A >= B) {
+        cout << A << " ";
+        return recursion1(A - 1, B);
+    }
+}
+
+int recursion2(int A, int B) {
+    if (A <= B) {
+        cout << A << " ";
+        return recursion2(A + 1, B);
+    }
+}
+
+int main() {
+    int A, B;
+    cin >> A;
+    cin >> B;
+    if (A >= B) recursion1(A, B);
+    else recursion2(A, B);
+}
+
