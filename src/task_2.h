13535d4f91f5d2e9493d0399ebd288d689211a19
Jasurbek <148448603+jasurbek030905@users.noreply.github.com>
2023-11-23 14:10:49 +0500

    Update task_2.h

--- src/task_2.h
+++ src/task_2.h
@@ -1,1 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int recursion(int A) {
+    if (A == 1) {
+        cout << "YES";
+    } else if (A % 2 == 1) {
+        cout << "NO";
+    }
+    if (A % 2 == 0) {
+        return recursion(A / 2);
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    recursion(n);
+}
+
 
